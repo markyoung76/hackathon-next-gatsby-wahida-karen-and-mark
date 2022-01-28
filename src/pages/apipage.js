d84ee2e377dfef37dabc9d77e2927b06f5f1884d
Mark Young <markyoung76@me.com>
2022-01-28 14:30:39 +0000

    Folder and file structure done, home page and api page basic layout completed

---
+++ src/pages/apipage.js
@@ -0,0 +1,13 @@
+import * as React from "react";
+
+const ApiPage = () => {
+  return (
+    <main>
+      <title>API Duck Image Generator</title>
+      <h1>Random Duck Image Generator</h1>
+      {/* <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p> */}
+    </main>
+  );
+};
+
+export default ApiPage;
