a33855ee117fcd36fedfac6d18378deba3c691ba
Mark Young <markyoung76@me.com>
2022-01-31 10:47:09 +0000

    styling added, button on api page still needs resolving

--- src/pages/apipage.js
+++ src/pages/apipage.js
@@ -1,32 +1,87 @@
-import React, {useState, useEffect} from "react";
-import CORS from 'cors'
+import React, { useState, useEffect } from "react";
+import { Link } from "gatsby";
+import { Button } from "antd";
 
-const fetch = require(`node-fetch`)
+// styles
 
+const homepage = {
+  height: "100vh",
+  width: "100vw",
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "center",
+  alignItems: "center",
+};
 
+const bkgdImage = {
+  backgroundImage: `url("https://wallpapercave.com/wp/wp4439888.jpg")`,
+  height: "100vh",
+  width: "100vw",
+};
+
+const heading = {
+  color: "#20b2aa",
+  fontSize: "60px",
+};
+
+const subheading = {
+  color: "lightblue",
+  fontSize: "30px",
+};
+
+const buttondiv = {
+  marginTop: "25px",
+};
+
+const button = {
+  color: "teal",
+  fontSize: "20px",
+  backgroundColor: "lightblue",
+  height: "50px",
+  borderRadius: "15px 15px 15px 15px",
+};
+
+const catpicture = {
+  height: "300px",
+  width: "300px",
+  borderStyle: "solid",
+  borderColor: "#20b2aa",
+  borderWidth: "8px",
+  borderRadius: "15px 15px 15px 15px",
+};
+
+// markup
+
+const fetch = require(`node-fetch`);
 
 const ApiPage = () => {
+  const [cat, setCat] = useState("");
 
-  const [duck, setDuck] = useState('');
+  useEffect(() => {
+    getCat();
+  }, []);
 
-useEffect(() => {
- getDuck();
-}, []);
-
-  const getDuck = async () => {
-    const response = await fetch(`https://aws.random.cat/meow`)
+  const getCat = async () => {
+    const response = await fetch(`https://aws.random.cat/meow`);
     const data = await response.json();
-    setDuck(data.file);
-console.log(data.file)
-    };
+    setCat(data.file);
+    console.log(data.file);
+  };
 
   return (
-    <main>
-      <title>API Duck Image Generator</title>
-      <h1>Random Duck Image Generator</h1>
-      {/* <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p> */}
-<img src={`${duck}`} alt="cat img"></img>
-    </main>
+    <div style={bkgdImage}>
+      <main style={homepage}>
+        <title>API Cat Image Generator</title>
+        <h1 style={heading}>Random Cat Image Generator</h1>
+        <p style={subheading}>Hi there! Have kittens, get clickin'</p>
+        <img style={catpicture} src={`${cat}`} alt="cat img"></img>
+        <div style={buttondiv}>
+          <Button style={button} type="secondary">
+            <Link to="/apipage">More Cats</Link>
+          </Button>
+        </div>
+      </main>
+    </div>
   );
 };
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,62 @@
 import * as React from "react";
-import { Link } from "gatsby"
+import { Link } from "gatsby";
 import { Button } from "antd";
+
+// styles
+
+const homepage = {
+  height: "100vh",
+  width: "100vw",
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "center",
+  alignItems: "center",
+};
+
+const bkgdImage = {
+  backgroundImage: `url("https://wallpapercave.com/wp/wp4439888.jpg")`,
+  height: "100vh",
+  width: "100vw",
+};
+
+const heading = {
+  color: "#20b2aa",
+  fontSize: "60px",
+};
+
+const subheading = {
+  color: "lightblue",
+  fontSize: "30px",
+};
+
+const buttondiv = {
+  marginTop: "25px",
+};
+
+const button = {
+  color: "teal",
+  fontSize: "20px",
+  backgroundColor: "lightblue",
+  height: "50px",
+  borderRadius: "15px 15px 15px 15px",
+};
+
+// markup
 
 const IndexPage = () => {
   return (
-    <main>
-      <title>API Duck Image Generator</title>
-      <h1>A Home For All Ducks</h1>
-      <p>No duck is ever left behind...</p>
-      <button>
-      <Link to="/apipage">Click for Ducks</Link>
-      </button>
-      <Button type="secondary">Primary</Button>
-    </main>
+    <div style={bkgdImage}>
+      <main style={homepage}>
+        <title>API Cat Image Generator</title>
+        <h1 style={heading}>A Home For All Cats</h1>
+        <p style={subheading}>No cat is ever left behind...</p>
+        <div style={buttondiv}>
+          <Button style={button} type="secondary">
+            <Link to="/apipage">Click for Cats</Link>
+          </Button>
+        </div>
+      </main>
+    </div>
   );
 };
 
